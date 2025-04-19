c5f470398805cc475e6a5582bd96204309f6bcd2
NIDHEESH TK <nidheeshnidi111@gmail.com>
2025-04-20 00:41:25 +0530

    modified html,css and script, alignment corrected with new design:CSS

--- pythonProject3/static/js/script.js
+++ pythonProject3/static/js/script.js
@@ -1,14 +1,36 @@
 function uploadImage() {
     const input = document.getElementById("imageInput");
     const file = input.files[0];
+    const label = document.getElementById("uploadLabel");
+
+    if (file) {
+        label.textContent = "✅ " + file.name;
+    } else {
+        label.textContent = "Choose Image";
+    }
 
     if (!file) {
         document.getElementById("result").textContent = "Please select an image.";
         return;
     }
+    
 
     const formData = new FormData();
     formData.append("image", file);
+
+
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        const preview = document.getElementById("preview");
+        preview.src = e.target.result;
+        preview.style.display = "block";
+    };
+    reader.readAsDataURL(file);
+
+    document.getElementById("classResult").textContent = "";
+    document.getElementById("probResult").textContent = "";
+    document.getElementById("resultCard").style.display = "none";
+    document.getElementById("result").textContent = "Processing... Please wait.";
 
     fetch("http://127.0.0.1:5000/predict", {
         method: "POST",
@@ -18,8 +40,14 @@ function uploadImage() {
         const data = await response.json();
 
         if (response.ok) {
-            document.getElementById("result1").textContent = "Predicted Class : " + data.predicted_class;
-            document.getElementById("result2").textContent = "Precition Accuracy: " + data.probability*100+ "%";
+            const formattedProb = (data.probability * 100).toFixed(2);
+            document.getElementById("classResult").innerHTML = `<strong>Predicted Class:</strong> <span>${data.predicted_class}</span>`;
+            document.getElementById("probResult").innerHTML = `<strong>Prediction Accuracy:</strong> <span>${formattedProb}%</span>`;
+
+            // show the card
+           
+            document.getElementById("resultCard").style.display = "block";
+            document.getElementById("result").textContent = ""; 
 
         } else {
             document.getElementById("result").textContent = "Error: " + JSON.stringify(data);
@@ -30,12 +58,5 @@ function uploadImage() {
         console.error("Fetch error:", error);
     });
 
-    // Show preview
-    const reader = new FileReader();
-    reader.onload = function(e) {
-        const preview = document.getElementById("preview");
-        preview.src = e.target.result;
-        preview.style.display = "block";
-    };
-    reader.readAsDataURL(file);
+
 }
